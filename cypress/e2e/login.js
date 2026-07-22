a17a42e5a44e97ec624536ea7a6cd91e07745461
Ghala Sudana <ghalasudana24@gmail.com>
2026-07-23 04:34:20 +0700

    update login.js

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -2,11 +2,18 @@ Cypress.Commands.add('Login', (email = Cypress.env('username'), password = Cypre
   // Klik tombol "Masuk"
   cy.get('#sign-in-btn').should('be.visible').click();
 
-  // Mengisi email
+  // Mengisi email dengan pemastian nilai terisi penuh
   cy.get('#_phone')
     .should('be.visible')
-    .clear()
-    .type(email, { delay: 50, parseSpecialCharSequences: false });
+    .then(($input) => {
+      const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+        window.HTMLInputElement.prototype,
+        'value'
+      ).set;
+      nativeInputValueSetter.call($input[0], email);
+      $input[0].dispatchEvent(new Event('input', { bubbles: true }));
+      $input[0].dispatchEvent(new Event('change', { bubbles: true }));
+    });
 
   // Klik tombol "Lanjutkan"
   cy.get('#btn-check-credential')
